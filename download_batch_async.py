560a9b51314d5123d76119eb736e0d6818e89221
Chia Yew Ken <chiayewken@gmail.com>
2019-04-18 22:27:50 +0800

    refactor

--- download_batch_async.py
+++ download_batch_async.py
@@ -5,6 +5,7 @@ import sys
 import tarfile
 import tempfile
 from pathlib import Path
+from typing import List
 
 import firebase_admin
 from aiohttp import ClientSession
@@ -36,7 +37,7 @@ from tqdm import tqdm
 #         return list(tqdm(pool.imap_unordered(newspaper_get_html, urls)))
 
 
-async def fetch(url, session):
+async def fetch(url: str, session) -> str:
     try:
         # timeout 7s -> 1min for 10k urls (4k success)
         # timeout 30s -> 3min for 10k urls (7k success)
@@ -46,13 +47,13 @@ async def fetch(url, session):
         return ""
 
 
-async def bound_fetch(sem, url, session):
+async def bound_fetch(sem: asyncio.Semaphore, url: str, session: ClientSession) -> str:
     # Getter function with semaphore.
     async with sem:
         return await fetch(url, session)
 
 
-async def fetch_htmls_loop(urls, hide_progress):
+async def fetch_htmls_loop(urls: List[str], hide_progress: bool) -> List[str]:
     # reference: https://pawelmhm.github.io/asyncio/python/aiohttp/2016/04/22/asyncio-aiohttp.html
     tasks = []
     # Semaphore to avoid python open file limit
@@ -77,31 +78,31 @@ async def fetch_htmls_loop(urls, hide_progress):
         return responses
 
 
-def fetch_htmls(urls, hide_progress):
+def fetch_htmls(urls: List[str], hide_progress: bool) -> List[str]:
     loop = asyncio.get_event_loop()
     future = asyncio.ensure_future(fetch_htmls_loop(urls, hide_progress))
     return loop.run_until_complete(future)
 
 
-def is_html(string):
+def is_html(string: str) -> bool:
     # return bool(BeautifulSoup(string, "html.parser").find())  # slow
     return "html" in string  # and len(string) > 1000
 
 
-def generate_name(suffix=""):
+def generate_name(suffix: str = "") -> str:
     tempfile.tempdir = tempfile.template = ""
     return tempfile.mktemp(suffix)
 
 
-def save_htmls(htmls, save_dir):
+def save_htmls(htmls: List[str], save_dir: Path) -> None:
     for h in htmls:
         if is_html(h):
             path = save_dir.joinpath(generate_name(".html"))
-            with open(path, "w") as file:
+            with open(str(path), "w") as file:
                 file.write(h)
 
 
-def archive(path_in, path_out, compress=True):
+def archive(path_in: Path, path_out: Path, compress: bool = True) -> Path:
     mode = {False: "w", True: "w:gz"}[compress]
     suffix = {False: ".tar", True: ".tgz"}[compress]
     path_out = path_out.with_suffix(suffix)
@@ -110,7 +111,7 @@ def archive(path_in, path_out, compress=True):
     return path_out
 
 
-def count_total_lines(file):
+def count_total_lines(file: str) -> int:
     count = 0
     with open(file) as f:
         for line in f:
@@ -118,37 +119,37 @@ def count_total_lines(file):
     return count
 
 
-def count_batches(total, batch_size):
+def count_batches(total: int, batch_size: int) -> int:
     return math.ceil(total / batch_size)
 
 
-def firebase_init(gdrive_dir):
+def firebase_init(gdrive_dir: Path) -> None:
     path_json, database_url = open(
-        gdrive_dir.joinpath("firebase_details.txt")
+        str(gdrive_dir.joinpath("firebase_details.txt"))
     ).readlines()
     cred = credentials.Certificate(str(gdrive_dir.joinpath(path_json.strip())))
     firebase_admin.initialize_app(cred, {"databaseURL": database_url.strip()})
     print("firebase initialized")
 
 
-def firebase_check_exists(id):
+def firebase_check_exists(id: str) -> bool:
     database = db.reference().get()
     # firebase acts like empty array when empty (nonetype iteration error)
     return database != None and str(id) in database
 
 
-def firebase_set(id, value):
+def firebase_set(id: str, value: bool) -> None:
     db.reference(str(id)).set(value)
 
 
-def get_batch_urls(urls_file, idx, batch_size):
+def get_batch_urls(urls_file: str, idx: int, batch_size: int) -> List[str]:
     # must avoid out of range error
     batch_range = range(idx * batch_size, (idx + 1) * batch_size)
     with open(urls_file) as f:
         return [x.strip() for (i, x) in enumerate(f) if i in batch_range]
 
 
-def firebase_sync_gdrive(gdrive_dir, save_dir):
+def firebase_sync_gdrive(gdrive_dir: Path, save_dir: Path) -> None:
     # manually align firebase entries to gdrive archives
     batch_names = [p for p in gdrive_dir.joinpath(save_dir).iterdir()]
     dict_new = {p.stem: True for p in batch_names}
@@ -158,15 +159,21 @@ def firebase_sync_gdrive(gdrive_dir, save_dir):
     db.reference().set(dict_new)
 
 
-def main(gdrive_dir, batch_size=100000, hide_tracebacks=True, hide_progress=True):
-    # should hide tqdm for long iterations because the output will slow down notebooks
+def main(
+    gdrive_dir: Path,
+    batch_size: int = 100000,
+    hide_tracebacks: bool = True,
+    hide_progress: bool = True,
+) -> None:
     """
-    init
-    get chunks
-    fetch htmls
-    save and archive
-    send to google drive
-    delete archive
+    Init firebase and setup directories
+    For every possible batch idx:
+        Check if exists in firebase
+        Get batch_size num lines of url strings from 2gb urls.txt in gdrive
+        Async fetch html contents
+        Save and archive
+        Send to google drive
+        Delete temp files (archive)
     """
     if hide_tracebacks:
         sys.tracebacklimit = 0  # suppress url error reports
@@ -176,7 +183,7 @@ def main(gdrive_dir, batch_size=100000, hide_tracebacks=True, hide_progress=True
     save_dir = Path("downloads")
     save_dir.mkdir(exist_ok=True)
     gdrive_dir.joinpath(save_dir).mkdir(exist_ok=True)
-    num_total_urls = count_total_lines(urls_file)
+    num_total_urls = count_total_lines(str(urls_file))
     num_total_batches = count_batches(num_total_urls, batch_size)
     print("num_total_urls:", num_total_urls)
     print("num_total_batches", num_total_batches)
@@ -189,10 +196,13 @@ def main(gdrive_dir, batch_size=100000, hide_tracebacks=True, hide_progress=True
 
         print("handling batch:", batch_name)
         firebase_set(batch_name, True)
-        urls = get_batch_urls(urls_file, batch_idx, batch_size)
+        urls = get_batch_urls(str(urls_file), batch_idx, batch_size)
+        # should hide tqdm for long iterations because the output can crash notebook
         htmls = fetch_htmls(urls, hide_progress)
         save_htmls(htmls, save_dir)
         archive_fname = archive(save_dir, Path(batch_name), compress=True)
-        shutil.move(archive_fname, gdrive_dir.joinpath(save_dir, archive_fname))
+        shutil.move(
+            str(archive_fname), str(gdrive_dir.joinpath(save_dir, archive_fname))
+        )
         shutil.rmtree(save_dir)
         save_dir.mkdir()
